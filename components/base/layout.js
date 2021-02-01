f7b07d4d21f129f81fd543a7b47cae1ae31ebfc1
Andova <andova86@gmail.com>
2021-02-01 18:10:57 -0500

    Arreglo em el nombre de la pagina

--- components/base/layout.js
+++ components/base/layout.js
@@ -11,13 +11,13 @@ export default function Layout({children}) {
                  <meta name="viewport"
                       content="width=device-width, initial-scale=1"/>
                  <meta property="og:title"
-                      content="Enjoy Cuba"
+                      content="Jack Construction"
                       key="cuba"/>
 
                 <meta name="title"
-                      content="Enjoy - Viajes a Cuba"/>
+                      content="Jack Construction"/>
                 <meta name="description"
-                      content="Descubre un país con alma, con mucho ritmo, vistas coloridas y rincones desconocidos"/>
+                      content="Jack Construction"/>
 
 
                 <meta property="og:type"
@@ -25,9 +25,9 @@ export default function Layout({children}) {
                 <meta property="og:url"
                       content="https://metatags.io/"/>
                 <meta property="og:title"
-                      content="Enjoy - Viajes a Cuba"/>
+                      content="Jack Construction"/>
                 <meta property="og:description"
-                      content="Descubre un país con alma, con mucho ritmo, vistas coloridas y rincones desconocidos"/>
+                      content="Jack Construction"/>
                 <meta property="og:image"
                       content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
 
@@ -37,19 +37,15 @@ export default function Layout({children}) {
                 <meta property="twitter:url"
                       content="https://metatags.io/"/>
                 <meta property="twitter:title"
-                      content="Enjoy - Viajes a Cuba"/>
+                      content="Jack Construction"/>
                 <meta property="twitter:description"
-                      content="Descubre un país con alma, con mucho ritmo, vistas coloridas y rincones desconocidos"/>
+                      content="Jack Construction"/>
                 <meta property="twitter:image"
                       content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
-                <title>Enjoy - Viajes a Cuba</title>
+                <title>Jack Construction - Building</title>
 
-                <link rel="preconnect"
-                      href="https://fonts.gstatic.com/"
-                      crossOrigin/>
 
-                <link rel="preload" href="http://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap" as="font"
-                      crossOrigin/>
+
 
             </Head>
 
--- components/cards/FooterData_card.js
+++ components/cards/FooterData_card.js
@@ -30,31 +30,7 @@ const Footerdata_card = () => {
             </div>
             <div className='footerdata_card_body'>
 
-                 <div className='columns is-mobile'>
-                     <div className='column company_card_footer_years is-half'>
 
-                    <div className='company_card_footer_years_number'>
-                        16
-                    </div>
-
-                      <div className='company_card_footer_years_text'>
-                          Years <br/> Established
-                    </div>
-                </div>
-
-                 <div className='column company_card_footer_projects is-half'>
-
-
-                    <div className='company_card_footer_projects_number '>
-                        100
-                    </div>
-
-                      <div className='company_card_footer_projects_text '>
-                          Completed <br/> Projects
-                    </div>
-                </div>
-
-                     </div>
 
 
             </div>
