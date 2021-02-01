2e50f4384323d25ad51c64c645e8c8cb19c9f1ce
Andova <andova86@gmail.com>
2021-02-01 15:11:40 -0500

    Add company Section

--- components/base/footer.js
+++ components/base/footer.js
@@ -4,227 +4,90 @@ import Image from 'next/image';
 const Footer = () => {
     return (
 
-        <section className="section has-background-white footer p-0">
-            <div className="container pt-0 pr-5 pb-0 pl-5">
-                <hr />
-                <div className="has-text-centered mt-6 mb-6 is-hidden-desktop">
-                    <figure className="image is-inline-block logo mb-3">
-                        <Link href='/'>
-                            <a>
-                                <Image src='/img/logo-footer.svg'
-                                    width={163}
-                                    height={40}
-                                    alt={'logo footer'} />
+        <footer className="py-3">
+            <div className="columns is-multiline">
+                <div className='column is-12 section'>
+                      <div className="navbar">
+                    <Link href="/">
+                        <a className="navbar-item is-uppercase is-active has-text-weight-medium">
+                            Home
+                        </a>
+                    </Link>
+
+
+                    <Link href="/">
+                        <a className="navbar-item is-uppercase">
+                            BUILD & REPAIR </a>
+                    </Link>
+
+                     <Link href="/">
+                        <a className="navbar-item is-uppercase">
+                            CUSTOM PROJECTS </a>
+                    </Link>
+
+
+                     <Link href="/">
+                        <a className="navbar-item is-uppercase">
+                            REMODELING </a>
+                    </Link>
+
+                     <Link href="/">
+                        <a className="navbar-item is-uppercase">
+                           OUR EXPERTISE </a>
+                    </Link>
+
+                    <Link href="/">
+                        <a className="navbar-item is-uppercase">
+                           COMPANY </a>
+                    </Link>
+
+                    <Link href="/">
+                        <a className="navbar-item is-uppercase">
+                           BLOG </a>
+                    </Link>
+
+                     <Link href="/">
+                        <a className="navbar-item is-uppercase">
+                           CONTACT US </a>
+                    </Link>
+
+                    <div className="navbar-item has-dropdown is-hoverable is-hidden">
+                        <a className="navbar-link">
+                            More
+                        </a>
+
+                        <div className="navbar-dropdown">
+                            <a className="navbar-item">
+                                About
                             </a>
-                        </Link>
-                    </figure>
-
-                    <div className="field has-text-grey-darker">
-                        <Link href='/'>
-                            <a>
-                                <span className="icon">
-                                    <i className="icon-twitter has-text-purple"
-                                        title="Twitter" />
-                                </span>
+                            <a className="navbar-item">
+                                Jobs
                             </a>
-                        </Link>
-
-                        <Link href='/'>
-                            <a>
-                                <span className="icon">
-                                    <i className="icon-facebook has-text-purple"
-                                        title="Facebook" />
-                                </span>
+                            <a className="navbar-item">
+                                Contact
                             </a>
-                        </Link>
-
-
-                        <Link href='/'>
-                            <a>
-                                <span className="icon">
-                                    <i className="icon-instagram has-text-purple"
-                                        title="Instagram" />
-                                </span>
+                            <hr className="navbar-divider"/>
+                            <a className="navbar-item">
+                                Report an issue
                             </a>
-                        </Link>
-
-
-                        <Link href='/'>
-                            <a>
-                                <span className="icon">
-                                    <i className="icon-linkedin has-text-purple"
-                                        title="Linkedin" />
-                                </span>
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className="columns mt-1 mb-1 is-flex-mobile">
-                    <div className="column is-5 is-hidden-mobile">
-                        <figure className="image logo mb-3">
-                            <Link href='/'>
-                                <a>
-                                    <Image src='/img/logo-footer.svg'
-                                        width={115}
-                                        height={45}
-                                        alt={'logo'} />
-                                </a>
-                            </Link>
-                        </figure>
-                        <div className="field has-text-grey-darker">
-
-                            <Link href='/'>
-                                <a>
-                                    <span className="icon">
-                                        <i className="icon-twitter has-text-purple"
-                                            title="Twitter" />
-                                    </span>
-
-                                </a>
-                            </Link>
-
-
-                            <Link href='/'>
-                                <a>
-                                    <span className="icon">
-                                        <i className="icon-facebook has-text-purple"
-                                            title="Facebook" />
-                                    </span>
-                                </a>
-                            </Link>
-
-
-                            <Link href='/'>
-                                <a>
-                                    <span className="icon">
-                                        <i className="icon-instagram has-text-purple"
-                                            title="Instagram" />
-                                    </span>
-                                </a>
-                            </Link>
-
-                            <Link href='/'>
-                                <a>
-                                    <span className="icon  ">
-                                        <i className="icon-linkedin has-text-purple"
-                                            title="Linkedin" />
-                                    </span>
-                                </a>
-                            </Link>
                         </div>
                     </div>
-                    <div className="column is-hidden-touch">
-                        {/* <h3 className="title">Productos</h3>
-                        <ul>
-                            <li>
-                                <Link href='/'>
-                                    <a>Hoteles</a>
-                                </Link>
-
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Casas Particulares</a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Excursiones</a>
-
-                                </Link></li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Traslados</a>
-
-                                </Link></li>
-                        </ul> */}
-                    </div>
-                    <div className="column">
-                        <h6 className="title">Enjoy</h6>
-                        <ul>
-                            <li>
-                                <Link href='/'>
-                                    <a>España</a>
-                                </Link></li>
-                            <li>
-                                <Link href='/'>
-                                    <a>México</a>
-                                </Link>
-
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Colombia </a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Italia </a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Portugal </a>
-                                </Link>
-
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Rusia </a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Chile
-                                    </a>
-                                </Link>
-
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="column">
-                        <h6 className="title">Más</h6>
-                        <ul>
-                            <li>
-                                <Link href='/'>
-                                    <a>About Us</a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/about'>
-                                    <a>Blog</a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Contact</a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a >Mapa del Sitio</a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Privacidad</a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/'>
-                                    <a>Terminos & Condiciones</a>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
+                </div>
+                <div className='column is-6-desktop has-bg-dark'>
+                </div>
+
+                  <div className='column is-6-desktop'>
+                </div>
+
+
             </div>
             <div className="p-2 has-bg-blue-footer">
                 <div className=" has-text-centered">
-                    <span className="font-10">© {new Date().getFullYear()} Enjoy Cuba. Todos los derechos reservados.</span>
+                    <span className="font-10">© {new Date().getFullYear()} Jack Construction. Todos los derechos reservados.</span>
                 </div>
             </div>
-        </section>
+        </footer>
 
     )
 }
--- components/cards/Company_card.js
+++ components/cards/Company_card.js
@@ -6,15 +6,21 @@ const Company_card = () => {
     return (
 
 
-        <div className='company_card'>
-            <div className='company_card_body'>
 
-                            <Image src='/img/company.png' layout={'responsive'} alt='house' width={1200} height={600} objectFit='cover'/>
+
+        <div className='company_card'>
+
+
+            <div className='company_card_header content_bg'>
+
+
 
 
             </div>
             <div className='company_card_footer'>
-                <div className='company_card_footer_years'>
+
+                 <div className='columns is-mobile'>
+                     <div className='column company_card_footer_years is-half'>
 
                     <div className='company_card_footer_years_number'>
                         16
@@ -24,6 +30,21 @@ const Company_card = () => {
                           Years <br/> Established
                     </div>
                 </div>
+
+                 <div className='column company_card_footer_projects is-half'>
+
+
+                    <div className='company_card_footer_projects_number '>
+                        100
+                    </div>
+
+                      <div className='company_card_footer_projects_text '>
+                          Completed <br/> Projects
+                    </div>
+                </div>
+
+                     </div>
+
 
             </div>
         </div>
